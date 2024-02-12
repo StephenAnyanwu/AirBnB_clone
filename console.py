5f0bcc02bba1dc515a1a72e694a4e1fbce332e37
StephenAnyanwu <voltasteve@gmailcom>
2024-02-11 18:18:56 -0800

    created console module

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""In this module defines a HBNBCommand class"""
+
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Impliment a command line interpreter"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command (Ctrl-D) to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing if nothing is passed"""
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
